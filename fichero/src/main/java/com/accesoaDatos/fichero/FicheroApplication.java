f0085acb9ecd02f0ccf57c540f9a5b235f52a116
David César Moro Nieto <davidmoro96@gmail.com>
2022-11-09 11:56:31 +0100

    Creacion del repositorio y del primer proyecto

---
+++ fichero/src/main/java/com/accesoaDatos/fichero/FicheroApplication.java
@@ -0,0 +1,13 @@
+package com.accesoaDatos.fichero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicheroApplication.class, args);
+	}
+
+}
